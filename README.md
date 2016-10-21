aa600c7de2235761741877a3bced19a3369ada1d
Daniel Alvarez <a3dany@gmail.com>
2016-10-20 20:15:45 -0400

    First

---
+++ README.md
@@ -0,0 +1,13 @@
+# BottomNavigationView Sample
+
+A simple example of component "BottomNavigationView"
+
+![Screenshot](https://cloud.githubusercontent.com/assets/1444991/19582357/c84411b6-9701-11e6-8519-f89e4c43f21c.png)
+
+## Dependencies
+
+`compile 'com.android.support:design:25.0.0'`
+
+## Resources
+
+* [Documentation](https://developer.android.com/reference/android/support/design/widget/BottomNavigationView.html)
---
+++ app/src/main/java/tech/alvarez/bottomnavigationviewsample/MainActivity.java
@@ -0,0 +1,44 @@
+package tech.alvarez.bottomnavigationviewsample;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView infoTextView;
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        infoTextView = (TextView) findViewById(R.id.infoTextView);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.inicioItem) {
+                    infoTextView.setText(R.string.inicio);
+                } else if (item.getItemId() == R.id.buscarItem) {
+                    infoTextView.setText(R.string.buscar);
+                } else if (item.getItemId() == R.id.camaraItem) {
+                    infoTextView.setText(R.string.camara);
+                } else if (item.getItemId() == R.id.favoritosItem) {
+                    infoTextView.setText(R.string.favoritos);
+                } else if (item.getItemId() == R.id.perfilItem) {
+                    infoTextView.setText(R.string.perfil);
+                }
+
+                return false;
+            }
+        });
+    }
+}
