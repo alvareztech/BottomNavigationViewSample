bb7e8a273116c811fce77525f9bb1bd969a7a52d
Daniel Alvarez <daniel@alvarez.tech>
2022-09-08 10:19:02 -0700

    General updates

--- app/src/main/java/tech/alvarez/bottomnavigationviewsample/MainActivity.java
+++ app/src/main/java/tech/alvarez/bottomnavigationviewsample/MainActivity.java
@@ -1,16 +1,23 @@
 package tech.alvarez.bottomnavigationviewsample;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import tech.alvarez.bottomnavigationviewsample.fragments.CameraFragment;
+import tech.alvarez.bottomnavigationviewsample.fragments.FavoritesFragment;
+import tech.alvarez.bottomnavigationviewsample.fragments.HomeFragment;
+import tech.alvarez.bottomnavigationviewsample.fragments.ProfileFragment;
+import tech.alvarez.bottomnavigationviewsample.fragments.SearchFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView infoTextView;
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -18,21 +25,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        infoTextView = findViewById(R.id.infoTextView);
+        final Fragment homeFragment = new HomeFragment();
+        final Fragment searchFragment = new SearchFragment();
+        final Fragment cameraFragment = new CameraFragment();
+        final Fragment favoritesFragment = new FavoritesFragment();
+        final Fragment profileFragment = new ProfileFragment();
+
+        if (savedInstanceState == null) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentContainer, homeFragment).commit();
+        }
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
+            FragmentManager fragmentManager = getSupportFragmentManager();
             if (item.getItemId() == R.id.inicioItem) {
-                infoTextView.setText(R.string.inicio);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, homeFragment).commit();
             } else if (item.getItemId() == R.id.buscarItem) {
-                infoTextView.setText(R.string.buscar);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, searchFragment).commit();
             } else if (item.getItemId() == R.id.camaraItem) {
-                infoTextView.setText(R.string.camara);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, cameraFragment).commit();
             } else if (item.getItemId() == R.id.favoritosItem) {
-                infoTextView.setText(R.string.favoritos);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, favoritesFragment).commit();
             } else if (item.getItemId() == R.id.perfilItem) {
-                infoTextView.setText(R.string.perfil);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, profileFragment).commit();
             }
             return true;
         });
---
+++ app/src/main/java/tech/alvarez/bottomnavigationviewsample/fragments/CameraFragment.java
@@ -0,0 +1,26 @@
+package tech.alvarez.bottomnavigationviewsample.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import tech.alvarez.bottomnavigationviewsample.R;
+
+public class CameraFragment extends Fragment {
+    public CameraFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_camera, container, false);
+    }
+}
